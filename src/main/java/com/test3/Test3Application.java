0370be53a9a82e10bcbe1d784d9dd26390225103
Udit Narayan Samal <uditnarayan9237@gmail.com>
2025-03-13 09:57:59 +0530

    First Message

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,12 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+                double z=10.3;
                 boolean isPresent=false;
-=======
-		double z=10.3;
->>>>>>> 73e8773a555bb58bdbe4d053ef67d772664b28f5
-		SpringApplication.run(Test3Application.class, args);
+                SpringApplication.run(Test3Application.class, args);
 	}
 
 }
