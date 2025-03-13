73e8773a555bb58bdbe4d053ef67d772664b28f5
Udit Narayan Samal <145748405+assistantudit09@users.noreply.github.com>
2025-03-13 09:36:48 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
+		double z=10.3;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
